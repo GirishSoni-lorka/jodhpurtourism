fe65c5260faf362dd43f41ff9717e5a3a95c417e
Girish <girish.soni.official@gmail.com>
2025-03-25 23:00:36 +0530

    First commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    @yield('head')
+<body>
+    @yield('header')
+    <div id="main">
+        @yield('content')
+    </div>
+    @yield('footer')
+</body>
+</html>
